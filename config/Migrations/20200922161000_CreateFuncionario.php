402529aed1f3996431c7604b55f60e7e8d29c9cb
SchindlerKael <schindler.baliza@outlook.com>
2020-12-01 00:22:32 -0300

    migrate funcionario

--- config/Migrations/20200922161000_CreateFuncionario.php
+++ config/Migrations/20200922161000_CreateFuncionario.php
@@ -6,17 +6,39 @@ require_once 'MigrationUtils.php';
 
 class CreateFuncionario extends AbstractMigration
 {
-    public function up()
+  public function up()
     {
-      //  $funcionario = $this->table('funcionarios');
-        //$funcionario
-         //   ->addColumn('nome','string',['limit'=>100])
-         //   ->addForeignKey('usuario_id','usuarios','id');
+        $funcionario = $this->table('funcionarios');
+        $funcionario
+        ->addColumn('nome', 'string', ['limit'=> 100])
+        ->addColumn('data_nasc','date')
+        ->addColumn('parentesco', 'string', ['limit'=>100])
+        ->addColumn('cpf', 'string', ['limit'=>16])
+        ->addColumn('rg', 'string', ['limit'=>16])
+        ->addColumn('profissao', 'string', ['limit'=>100])
+        ->addColumn('endereco_id', 'integer', ['limit'=>10])
+        ->addForeignKey('endereco_id', 'enderecos', 'id')
+        ->addColumn('telefone_id', 'integer', ['limit'=>10])
+        ->addForeignKey('telefone_id', 'telefones', 'id')
+
+        ->addColumn('data_criacao','datetime',['default'=>'CURRENT_TIMESTAMP'])
+        ->addColumn('data_modificacao','datetime',['default'=>'CURRENT_TIMESTAMP'])
+        ->addColumn('data_delecao','datetime',['null'=>true])
+        ->create();
+       
     }
 
     public function down()
     {
 
+        if(DEVELOPMENT){
+            $tableNames = ['funcionarios'];
+            $util = new MigrationUtils();
+            $util->checkDropAll($this,$tableNames);
+        } else {
+            print("Nao eh possivel deletar as tabelas arquivos. DEVELOPMENT: false");
+            throw new Exception("Impossivel Reversao");
+        }
     
     }
 }
